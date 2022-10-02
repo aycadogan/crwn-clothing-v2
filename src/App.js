e94f42906602b422a4e02033bb400131be8ff143
aycadogan89 <doganayca89@gmail.com>
2022-10-02 02:35:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        <img />
         <div className='category-body-container'>
           <h2>Jackets</h2>
           <p>Shop Now</p>
@@ -37,7 +37,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
